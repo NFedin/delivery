785793c80087cefe1b17997157d58b7cab560b33
Nikita Fedin <niks1995.fedin@gmail.com>
2022-07-09 15:41:56 +0300

    fixes

--- src/main/java/com/nikfedin/delivery/models/Delivery.java
+++ src/main/java/com/nikfedin/delivery/models/Delivery.java
@@ -3,7 +3,6 @@ package com.nikfedin.delivery.models;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Builder
--- src/main/java/com/nikfedin/delivery/repositories/DeliveryRepository.java
+++ src/main/java/com/nikfedin/delivery/repositories/DeliveryRepository.java
@@ -1,23 +1,14 @@
 package com.nikfedin.delivery.repositories;
 
 import com.nikfedin.delivery.models.Delivery;
-import com.nikfedin.delivery.models.Status;
 import com.nikfedin.delivery.models.Timeslot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-//    List<Delivery> findByStatus(Status stat);
-//
-//    List<Delivery> findAllByStatusNot(Status status);
-//
-//    List<Delivery> findAllByStartDateLessThan(LocalDateTime today);
-//
-//    List<Delivery> findAllByStartDateBetween(LocalDateTime start, LocalDateTime end);
 
     List<Delivery> findAllByTimeslotIn(List<Timeslot> timeslots);
 }
